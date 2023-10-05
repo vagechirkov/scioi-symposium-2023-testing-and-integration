e1eec8065029460ef33d20538594b59a71cb4b82
valery <vagechirkov@gmail.com>
2023-10-05 16:04:04 +0200

    implement median funtion

--- my_stats.py
+++ my_stats.py
@@ -1,6 +1,16 @@
 def my_median(my_list):
-    # first version of median function (not correct)
-    return sum(my_list) / len(my_list)
+    # sort the list
+    my_list.sort()
+    # get the length of the list
+    length = len(my_list)
+    # if the length is odd
+    if length % 2 == 1:
+        # return the middle element
+        return my_list[length // 2]
+    # if the length is even
+    else:
+        # return the average of the middle two elements
+        return (my_list[length // 2 - 1] + my_list[length // 2]) / 2
 
 
 def my_mean(my_list):
