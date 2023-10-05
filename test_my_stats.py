6470539a89ff0005a311600708f8cc2246917c91
valery <vagechirkov@gmail.com>
2023-10-05 16:37:58 +0200

    use pytest parametrize

--- test_my_stats.py
+++ test_my_stats.py
@@ -5,24 +5,15 @@ import pytest
 from my_stats import my_median
 
 
-def test_my_median_basic():
-    data = [1, 2, 3, 4, 5]
+@pytest.mark.parametrize("data, expected", [
+    ([1, 2, 3, 4, 5], 3),
+    ([1, 2, 3, 4, 5, 6], 3.5),
+    ([1, 6, 17], 6),
+    ([1, 6, 17, 2, 3], 3),
+])
+def test_my_median_basic(data, expected):
     result = my_median(data)
-    assert result == 3
-
-    data = [1, 2, 3, 4, 5, 6]
-    result = my_median(data)
-    assert result == 3.5
-
-    # test case when median is not equal to mean
-    data = [1, 6, 17]
-    result = my_median(data)
-    assert result == 6
-
-    # test case when data are not sorted
-    data = [1, 6, 17, 2, 3]
-    result = my_median(data)
-    assert result == 3
+    assert result == expected
 
 
 def test_my_median_empty():
