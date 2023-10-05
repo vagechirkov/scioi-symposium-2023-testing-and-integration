c66ecd365607744db7be37fe943438a37deb0b14
valery <vagechirkov@gmail.com>
2023-10-05 15:15:53 +0200

    initial setup

---
+++ my_stats.py
@@ -0,0 +1,10 @@
+def my_median(my_list):
+    raise NotImplementedError
+
+
+def my_mean(my_list):
+    raise NotImplementedError
+
+
+def my_variance(my_list):
+    raise NotImplementedError
---
+++ test_my_stats.py
@@ -0,0 +1,2 @@
+def test_my_median():
+    pass
