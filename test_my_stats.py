928558aaf06a5419d6596eab57ee8d1a1c863426
valery <vagechirkov@gmail.com>
2023-10-05 15:26:48 +0200

    write basic test for my_median function

--- test_my_stats.py
+++ test_my_stats.py
@@ -1,2 +1,9 @@
-def test_my_median():
-    pass
+from my_stats import my_median
+
+
+def test_my_median_basic():
+    data = [1, 2, 3, 4, 5]
+    assert my_median(data) == 3
+
+    data = [1, 2, 3, 4, 5, 6]
+    assert my_median(data) == 3.5
